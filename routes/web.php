ea732884fb8e58cfde95971c14e7b6b25faefc9e
Emmanuel_Lopez97 <jesus.ortiz@carsol.mx>
2025-07-15 13:32:29 -0600

    Capitulo 14
    Generacion de rutas y vistas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return "Welcome";
+});
+
+Route::get('/testView', function () {
+    return view('test');
+});
+
+Route::get('/crudExample', function () {
+    $age = 28;
+    $data = ['name' => 'Jesus Lopez', 'age' => $age];
+
+    return view('crud/index', $data);
+})->name('crud');
